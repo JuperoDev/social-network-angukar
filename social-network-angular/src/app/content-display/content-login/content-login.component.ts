084df56e7f827e19afdd03381c33f773839d2eef
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-11-06 13:59:04 +0100

    minimal angular social

---
+++ social-network-angular/src/app/content-display/content-login/content-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-login',
+  templateUrl: './content-login.component.html',
+  styleUrl: './content-login.component.scss'
+})
+export class ContentLoginComponent {
+
+}
---
+++ social-network-angular/src/app/content-display/content-user/content-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-user',
+  templateUrl: './content-user.component.html',
+  styleUrl: './content-user.component.scss'
+})
+export class ContentUserComponent {
+
+}
